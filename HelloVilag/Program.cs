4cf807df5915d7753ec09b26875ae6b656be8be7
gyure <gyure@SZODASPC>
2025-12-06 16:07:57 +0100

    Add project files.

---
+++ HelloVilag/Program.cs
@@ -0,0 +1,9 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseUrls("http://0.0.0.0:8080");
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello Világ :)");
+app.MapGet("/welcome/{name}", (string name) => $"Welcome, {name}!");
+
+app.Run();
